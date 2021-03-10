1c8ec6f7d507cc49ca2e39266fdb8f2a5044ec23
Lucas de Queiroz <lucasqreis@poli.ufrj.br>
2021-03-10 10:24:05 -0300

    challenge 1 done

---
+++ README.md
@@ -0,0 +1,9 @@
+Hello World!
+
+These are my solution links to the following FCC Certification:
+
+https://repl.it/@QIRoss/boilerplate-mean-variance-standard-deviation-calculator-1
+
+Link to my FCC Certification:
+
+Hope you enjoy it, Lucas de Queiroz :)
---
+++ fcc1.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def calculate(list):
+    if len(list) < 9:
+        raise ValueError("List must contain nine numbers.")
+    list = np.array(list).reshape(3,3)
+    return {
+        'mean': [list.mean(axis=0).tolist(),list.mean(axis=1).tolist(),list.mean()],
+        'variance': [list.var(axis=0).tolist(),list.var(axis=1).tolist(),list.var()],
+        'standard deviation': [list.std(axis=0).tolist(),list.std(axis=1).tolist(),list.std()],
+        'max' : [list.max(axis=0).tolist(),list.max(axis=1).tolist(),list.max()],
+        'min' : [list.min(axis=0).tolist(),list.min(axis=1).tolist(),list.min()],
+        'sum' : [list.sum(axis=0).tolist(),list.sum(axis=1).tolist(),list.sum()]
+    }
