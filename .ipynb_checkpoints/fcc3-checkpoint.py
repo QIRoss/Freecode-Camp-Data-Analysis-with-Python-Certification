e4f3675743c0b49731c83989c1020b752355bb30
Lucas de Queiroz <lucasqreis@poli.ufrj.br>
2021-03-11 12:19:15 -0300

    All tests passed but done much like a  and I rather use numpy

--- .ipynb_checkpoints/fcc3-checkpoint.py
+++ .ipynb_checkpoints/fcc3-checkpoint.py
@@ -25,7 +25,6 @@ def normalizeGluc(row):
     if row['gluc'] == 1:
         return 0
     return 1
-
 df['cholesterol']   = df.apply(lambda row: normalizeCholesterol(row),axis=1)
 df['gluc']          = df.apply(lambda row: normalizeGluc(row),axis=1)
 
@@ -35,10 +34,11 @@ def draw_cat_plot():
     df_cat = None
 
     # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the collumns for the catplot to work correctly.
-    df_cat = df.melt(id_vars=['cardio'], value_vars=['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke'])
+    df_cat = df.melt(id_vars=['cardio'], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active','overweight'])
 
     # Draw the catplot with 'sns.catplot()'
     fig = sns.catplot(x="variable", kind = "count", data=df_cat, hue = 'value', col= 'cardio').set_axis_labels('variable', 'total').fig
+
 
     # Do not modify the next two line
     fig.savefig('catplot.png')
@@ -48,26 +48,22 @@ def draw_cat_plot():
 # Draw Heat Map
 def draw_heat_map():
     # Clean the data
-    df_heat = df[(df['ap_lo'] <= df['ap_hi']) 
-    & (df['height'] >= df['height'].quantile(0.025)) 
-    & (df['height'] <= df['height'].quantile(0.975)) 
-    & (df['weight'] >= df['weight'].quantile(0.025))
-    & (df['weight'] <= df['weight'].quantile(0.975))]
+    df_heat = None
 
     # Calculate the correlation matrix
-    corr = df_heat.corr()
+    corr = None
 
     # Generate a mask for the upper triangle
-    mask = np.triu(np.ones_like(corr, dtype=bool))
+    mask = None
 
 
     # Set up the matplotlib figure
-    fig, ax = plt.subplots(figsize=(11, 9))
+    # fig, ax = None
 
     # Draw the heatmap with 'sns.heatmap()'
 
-    sns.heatmap(corr, mask = mask, vmax = .3, center = 0, square = True, linewidths = .5, cbar_kws = {"shrink": .8}, annot = True, fmt = ".1f")
+
 
     # Do not modify the next two lines
-    fig.savefig('heatmap.png')
-    return fig
+    # fig.savefig('heatmap.png')
+    # return fig
